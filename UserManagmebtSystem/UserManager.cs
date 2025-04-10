5277bd225076ed775e5e4ee14c5fd03608a64057
вадим заикин <zaikinvadim2017@gmail.com>
2025-04-10 19:40:32 +0700

    Добавлен базовый класс UserManager

---
+++ UserManagmebtSystem/UserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmebtSystem
+{
+    public class UserManager
+    {
+        List _users = new List();
+
+        public void AddUser(string username)
+        {
+            _users.Add(username);
+        }
+    }
+}
